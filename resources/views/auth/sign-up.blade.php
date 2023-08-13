07f3438fc1c4124cb23ed7949b6c51fbb0e2eed2
learn-and-conquer <alexander.webtech@yandex.ru>
2023-08-13 14:08:39 +0300

    Users-management scaffolding

---
+++ resources/views/auth/sign-up.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <form method="post" action="{{ route('auth.sign-up') }}">
+
+    </form>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<body>
+<div style="width: 80%; margin-left: auto; margin-right: auto;">
+@yield('content')
+</div>
+</body>
+</html>
