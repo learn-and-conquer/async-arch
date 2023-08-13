4670eda013ce2fc1fee7fac3b993f8bd77a8a1ac
learn-and-conquer <alexander.webtech@yandex.ru>
2023-08-13 16:34:29 +0300

    User model changed in tests

--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Auth;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,6 +10,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $connection = 'pgsql_auth';
 
     /**
      * The attributes that are mass assignable.
